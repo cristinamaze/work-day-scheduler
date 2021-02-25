523e01a066efdaec89bb8a70ae876760025cd25d
cristinamaze <76184003+cristinamaze@users.noreply.github.com>
2021-02-25 13:51:08 -0500

    change file structure

---
+++ script.js
@@ -0,0 +1,16 @@
+//Current Date and Time//
+var today = moment();
+$("#currentDay").text(today.format("[It is currently:] MMMM Do YYYY, h:mm:ss a"));
+console.log.this
+
+
+
+//DOM element//
+
+//Calendar Details//
+
+//Container//
+
+//Moment JS//
+
+//For Loop//
