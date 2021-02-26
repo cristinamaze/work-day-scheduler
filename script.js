62591ea8c9c060a261bcecb3baf967a5262762a3
cristinamaze <76184003+cristinamaze@users.noreply.github.com>
2021-02-26 16:35:40 -0500

    timeCheck function

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
+$(document).ready(function () {
+
 //Current Date and Time//
 var today = moment();
 $("#currentDay").text(today.format("[It is currently:] MMMM Do YYYY, h:mm a"));
 
 //Pull from Local Storage//
-localStorage.setItem("input-text", JSON.stringify(input-text));
-localStorage.getItem("input-text")
+// localStorage.setItem("input-text", JSON.stringify(input-text));
+// localStorage.getItem("input-text")
 
-//Save Button & click event//
-$(document).ready(function () {
-})
+// //Save Button & click event//
+// $(document).ready(function () {
 
-//Move through Timeblocks//
-$(document).ready(function (){
-})
+// })
 
-//Timeblock Color Display//
-if (timeblock === currentHour) {
+// saveBtn.on("click", function(){
 
-};
-else if (timeblock < currentHour) {
+// })
 
-};
-else (timeblock > currentHour) {
-};
+//Check Time//
+let timeCheck = function() {
+    var currentTime = moment().hour();
+
+    $(".input-text").each(function(){
+        var timeblock = parseInt($(this).attr("id"));
+        console.log(timeblock, currentTime)
+    
+
+
+    //Timeblock Color Display//
+    if (timeblock === currentTime) {
+        $(this).addClass("present")
+    }
+
+    else if (timeblock < currentTime) {
+        $(this).addClass("past")
+    }
+
+    else {
+        $(this).addClass("future")
+    };
+    })
+}; 
 
 //Reset Each Day//
+// var currentDay = moment().format("MMMM Do YYYY");
+
+timeCheck()
+})
