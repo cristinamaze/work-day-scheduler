5742f0338784082a967a7031e9ddf603bcd5b2d4
cristinamaze <76184003+cristinamaze@users.noreply.github.com>
2021-02-26 11:31:09 -0500

    framing

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 //Current Date and Time//
 var today = moment();
-$("#currentDay").text(today.format("[It is currently:] MMMM Do YYYY, h:mm:ss a"));
-console.log.this
+$("#currentDay").text(today.format("[It is currently:] MMMM Do YYYY, h:mm a"));
 
+//Pull from Local Storage//
+localStorage.setItem("input-text", JSON.stringify(input-text));
+localStorage.getItem("input-text")
 
+//Save Button & click event//
+$(document).ready(function () {
+})
 
-//DOM element//
+//Move through Timeblocks//
+$(document).ready(function (){
+})
 
-//Calendar Details//
+//Timeblock Color Display//
+if (timeblock === currentHour) {
 
-//Container//
+};
+else if (timeblock < currentHour) {
 
-//Moment JS//
+};
+else (timeblock > currentHour) {
+};
 
-//For Loop//
+//Reset Each Day//
